cfdccbd2405bd0f6dbaa3d679e31e1a1b498f2d9
Javilejo <pra21486@uvg.edu.gt>
2024-04-13 20:52:06 -0600

    add delete game by id

--- api/routes/gamesR.js
+++ api/routes/gamesR.js
@@ -4,4 +4,5 @@ const router = Router();
 router.post('/new', gamesC.newGame);
 router.put('/edit/:gameId', gamesC.editGamefields);
 router.delete('/delete/:gameId', gamesC.deleteGamefields);
+router.delete('/deleteGames/:gameId', gamesC.deleteGame);
 module.exports = router;
